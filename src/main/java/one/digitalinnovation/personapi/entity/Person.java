e7d6d21b3a518ffa2b4253971448d1f308318135
Rafael <rafaa.verdao@gmail.com>
2021-09-26 01:06:29 -0300

    create entities Person, Phone and enum PhoneType

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -0,0 +1,39 @@
+package one.digitalinnovation.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE,
+            CascadeType.REMOVE})
+    private List<Phone> phones;
+}
